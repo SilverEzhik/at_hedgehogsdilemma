700ab12220bc1190126701c9419fbc7299a90da1
jae kap <me@jkap.io>
2017-08-01 15:35:46 -0700

    changed username and tweet body to prefer Times New Roman Condensed
    it's closer to the title card font
    
    I would embed the font but it's against license and i don't wanna get this dmca'd

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ def chrip():
 
     #good people use templates but we are not good people
 
-    page = "<!DOCTYPE HTML><html><head><title>@" + username + "</title><style>html{transform-origin:top;transform:scale(1,1.5)}body{margin:0;background-color:#000;color:#fff}.chirp:nth-child(even){background-color:#070707}.chirp{width:100vw;height:50vh;padding-top:10vh;padding-bottom:15vh}.container{margin:0 auto;padding-bottom:20vh;width:80%;height:60vh}.username{height:60%;font-size:20vh;word-wrap:break-word;line-height:16vh}.episode{height:10%;font-family:sans-serif;font-size:5vh;font-weight:700}.tweet{height:30%;font-size:5vh}</style></head><body>"
+    page = "<!DOCTYPE HTML><html><head><title>@" + username + "</title><style>html{transform-origin:top;transform:scale(1,1.5)}body{margin:0;background-color:#000;color:#fff}.chirp:nth-child(even){background-color:#070707}.chirp{width:100vw;height:50vh;padding-top:10vh;padding-bottom:15vh}.container{margin:0 auto;padding-bottom:20vh;width:80%;height:60vh}.username{height:60%;font-size:20vh;word-wrap:break-word;line-height:16vh;font-family:Times NR Condensed,serif;font-weight:bold;}.episode{height:10%;font-family:sans-serif;font-size:5vh;font-weight:700}.tweet{height:30%;font-size:5vh;font-family:Times NR Condensed,serif;font-weight:bold;}</style></head><body>"
 
     for tweet in tweets:
         page += "<div class=chirp><div class=container><div class=username>@" + tweet.user.screen_name + "</div><div class=episode>EPISODE:<span class=date>" + datetime.fromtimestamp(tweet.created_at_in_seconds).strftime('%Y%m%d') + "</span></div><div class=tweet><span class=pronoun>" + pronoun + "</span> said,\"<span class=tweet>" + tweet.text + "</span>\"</div></div></div>"
